10f2b29a39a1ea851f98151241fe8dd834ae3a0b
taibarajabali345 <156583263+taibarajabali345@users.noreply.github.com>
2024-05-19 14:12:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,231 @@
+alert("Error! please enter the valid password");
+
+alert("HELLO! happy coding");
+
+alert("werlcome to js world")
+
+alert("Hello! this message is generated through the browser's developer console!");
+
+//  Chapter 2
+document.write('<h3>Chapter 2</h3>')
+var username;
+var myName = "Your Full Name";
+ 
+var message = "Hello! World";
+alert(message);
+ 
+var studentName = "taiba rajab ali";
+var age = 11;
+var gender = "female";
+var country ="pakistan";
+
+ alert("Name: " + studentName);
+alert("Age: " + age);
+alert("Gender: " + gender);
+alert("Country: " + country);
+alert('Certified Mobile Application Development')
+alert('PIZZA',"</br>")
+alert('PIZZ',"</br>" )
+alert('PIZ',"</br>")
+alert('PI',"</br> ",)
+alert('P',"</br> " )
+
+alert('My email address is taibarajabali5@gmail.com')
+alert(' I am trying to learn from the book smarter way to learn JavaScript')
+document.write('YAH! I can write HTML content through Javascript')
+
+var specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+alert(specialString);
+
+document.write('<h5>Chapter 3</h5>')
+alert('You have visited this site 15 times');
+document.write('<p>My Birth Year Is 2012</p>')
+var visitorName = "Taiba Rajab Ali";
+var productTitle = "T-shirt";
+var quantity = 5;
+var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.";
+alert(message);
+// Chapter 4
+document.write('<h2>Rules for naming JS variables</h2>');
+document.write(' <h3><strong>Chapter 4</strong></h3>');
+document.write(' <p>Variable names can only contain letters, numbers, underscores, and dollar signs. For example $my_1stVariable.</p> ');
+document.write(' <p>Variables must begin with a letter, underscore, or dollar sign. For example $name, _name, or name.</p> ');
+document.write(' <p>Variable names are case-sensitive.</p> ')
+document.write(' <p>Variable names should not be JS keywords.</p> ')
+
+// chpter 5
+document.write(' <u><h4>Chapter 5</h4></u>');
+
+var number;
+
+
+number = 5;
+
+document.write("Initial value: " + number + "<br>");
+
+
+number++;
+
+document.write("Value after increment is: " + number + "<br>");
+
+number += 7;
+
+document.write("Value after addition is: " + number + "<br>");
+
+number--;
+
+document.write("Value after decrement is: " + number + "<br>");
+
+var remainder = number % 3;
+
+document.write("The remainder is: " + remainder);
+
+alert(107+10);
+alert(10000-100);
+alert(12%7);
+alert(11*12);
+
+document.write(' <strong><u>Chapter5</strong></u> ')
+document.write(' <p><strong>Cost of one movie ticket is 300PKR so the cost of 5 ticket is 3000PKR</strong></p> ')
+
+ document.write( '<h6>Table Of 5</h6>' )
+document.writeln('5 x 1 = ', 5*1, '<br>')
+document.write('5 x 2 = ', 5*2, '<br>')
+document.write('5 x 3 = ', 5*3, '<br>')
+document.write('5 x 4 = ', 5*4, '<br>')
+document.write('5 x 5 = ', 5*5, '<br>')
+document.write('5 x 6 = ', 5*6, '<br>')
+document.write('5 x 7 = ', 5*7, '<br>')
+document.write('5 x 8 = ', 5*8, '<br>')
+document.write('5 x 9 = ', 5*9, '<br>')
+document.write('5 x 10 = ', 5*10, '<br>')
+
+document.write( '<u><h3>Shopping Cart</h3></u>' )
+document.write( 'Price of item 1 is 650</br>' )
+document.write( 'Quantity of 1 item 3</br>' )
+document.write( 'Price of item 2 is 100</br>' )
+document.write( 'Quantity of item 2 is 7</br>' )
+document.write( 'Shopping charges 100</br>' )
+document.write( '</br>' )
+document.write( 'Total cost of your order is 2750' )
+
+document.write( '<h4>Marks Sheet</h4>' )
+document.write( 'Total marks: 980 </br>' )
+document.write( 'Obtained marks: 804</br>' )
+document.write( 'Percentage: 82.0408163265306%</br>' )
+
+document.write('<h5>Currensy in PKR</h5>')
+document.write('1 US Dollar = 104.80 pakistani rupee</br>')
+document.write('1 Saudi Riyal = 28 Pakistani Rupee</br>')
+document.write('10 US Dollar = 2781.39 Pakistani rupees</br>')
+document.write('25 Saudi Riyal = 1854.03 Pakistani rupees</br>')
+
+var initialNumber = 10;
+var result = ((initialNumber + 5) * 10) / 2;
+console.log("Result: " + result);
+
+ document.write( '<h3>Age calculator</h3>' )
+document.write( 'Current year: 2024 </br>' )
+document.write( 'Birth year: 2012</br>' )
+document.write( 'My age is 11</br>' )
+
+document.write('<h5>The Geometrized</h5>')
+document.write('Radius of circle= 20</br>')
+document.write('The Circumference is= 125.67999999999999</br>')
+document.write('The area is = 1256.8</br>')
+
+document.write('<h2>The LifeTime Supply Calculator</h2></br>')
+document.write('Favourite snacks: Chips</br>')
+document.write('Current Age: 11</br>')
+document.write('Estimated Maximum Age: 55</br>')
+document.write('Amount Of Snack per Day: 5</br>')
+document.write('You will need 150 Chips to last you until the ripe old age of 55</br>')
+
+let usDollars = 10;
+let saudiRiyals = 25;
+
+// Exchange rates
+const usdToPkrRate = 104.80;
+const sarToPkrRate = 28;
+
+// Calculate total amount in Pakistani Rupees
+let totalPkr = (usDollars * usdToPkrRate) + (saudiRiyals * sarToPkrRate);
+
+// Output the result
+console.log("Total amount in Pakistani Rupees: " + totalPkr);
+
+document.write('<h3>CHAPTER 6-9</h3>')
+document.write('<strong>Result</strong></br>')
+document.write('<b>The value of a is: 10</b></br>')
+document.write('<hr></br>')
+document.write('The value of ++a is: 11</br>')
+document.write('Now the value of a is: 11</br>')
+document.write('</br></br>')
+document.write('The value of a++ is: 11</br>')
+document.write('Now the of a is: 12</br>')
+document.write('</br></br>')
+document.write('The value of --a is: 11</br>')
+document.write('Now the value of a is: 11</br>')
+document.write('</br></br>')
+document.write('The value of a-- is: 11</br>')
+document.write('Now the value of a is: 10</br>')
+ document.write('</br>')
+
+ document.write('<h5>Answer 2</h5>')
+ var a = 2, b = 1;
+var result = --a - --b + ++b + b--;
+// Display the values using document.write
+document.write("a is " + a + "<br>");
+document.write("b is " + b + "<br>");
+document.write("result is " + result + "<br>");
+
+document.write('<h3>Answer 3-4</h3>')
+// Prompt the user to enter their name
+var userName = prompt("Enter your name:");
+
+// Greet the user
+if (userName) {
+    alert("Hello, " + userName + "! Welcome to our website.");
+} else {
+    alert("Hello! Welcome to our website.");
+}
+// Prompt the user to enter a number
+var userInput = prompt("Enter a number:");
+
+// Parse the user input as an integer or default to 5
+var number = parseInt(userInput) || 5;
+
+// Generate and display the multiplication table
+document.write("<h2>Multiplication Table of " + number + "</h2>");
+document.write("<table>");
+for (var i = 1; i <= 10; i++) {
+    document.write("<tr><td>" + number + " x " + i + "</td><td>=</td><td>" + (number * i) + "</td></tr>");
+}
+
+
+var subject1 = prompt("Enter the name of the first subject:");
+var subject2 = prompt("Enter the name of the second subject:");
+var subject3 = prompt("Enter the name of the third subject:");
+
+// b) Total marks for each subject is 100, store it in another variable.
+var totalMarks = 100;
+
+// c) Take obtained marks for first subject from user and stored it in different variable.
+var obtainedMarks1 = parseInt(prompt("Enter the obtained marks for " + subject1 + ":"));
+
+// d) Take obtained marks for remaining 2 subjects from user and store them in variables.
+var obtainedMarks2 = parseInt(prompt("Enter the obtained marks for " + subject2 + ":"));
+var obtainedMarks3 = parseInt(prompt("Enter the obtained marks for " + subject3 + ":"));
+
+// e) Now calculate total marks and percentage and show the result in browser
+var totalObtainedMarks = obtainedMarks1 + obtainedMarks2 + obtainedMarks3;
+var percentage = (totalObtainedMarks / (totalMarks * 3)) * 100;
+
+document.write("<h2>Results</h2>");
+document.write("<p>Subject 1: " + subject1 + "<br>Obtained Marks: " + obtainedMarks1 + "</p>");
+document.write("<p>Subject 2: " + subject2 + "<br>Obtained Marks: " + obtainedMarks2 + "</p>");
+document.write("<p>Subject 3: " + subject3 + "<br>Obtained Marks: " + obtainedMarks3 + "</p>");
+document.write("<p>Total Obtained Marks: " + totalObtainedMarks + "/" + (totalMarks * 3) + "</p>");
+document.write("<p>Percentage: " + percentage.toFixed(2) + "%</p>");
+
